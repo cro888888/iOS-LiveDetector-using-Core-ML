28c62cc2d29dd5f64708ff6269b7ea0e359234d0
cro888888 <cro888888@gmail.com>
2017-11-04 10:35:15 +0800

    Add files via upload

--- LiveDetector/LiveDetector/ViewController.swift
+++ LiveDetector/LiveDetector/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import AVFoundation
 
 
+@available(iOS 11.0, *)
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
-    
+    let model = captain_cnn()
     var previewView : UIView!
     var boxView:UIView!
     
@@ -23,20 +24,15 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     let session = AVCaptureSession()
     var currentFrame: UIImage!
     var done = false
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         previewView = UIView(frame: CGRect(x: 0, y: 0, width:  UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
         previewView.contentMode = UIViewContentMode.scaleAspectFit
         view.addSubview(previewView)
-        
-        //Add a box view
-        //boxView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 600))
-        //boxView.backgroundColor = UIColor.green
-        //boxView.alpha = 0.3
-        //view.addSubview(boxView)
-        
         self.setupAVCapture()
+        timer = Timer.scheduledTimer(timeInterval: 0.07, target: self, selector:(#selector(ViewController.pred)), userInfo: nil, repeats: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -108,12 +104,10 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         session.startRunning()
     }
     
-    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!, sender: Any) {
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutput sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
         currentFrame =   convert(cmage: self.convertImageFromCMSampleBufferRef(sampleBuffer))
     }
     
-    
-    // clean up AVCapture
     func stopCamera(){
         session.stopRunning()
         done = false
@@ -124,6 +118,32 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         let ciImage:CIImage = CIImage(cvImageBuffer: pixelBuffer)
         return ciImage
     }
+    
+    func buffer(from image: UIImage) -> CVPixelBuffer? {
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: image.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+    
     func convert(cmage:CIImage) -> UIImage
     {
         let context:CIContext = CIContext.init(options: nil)
@@ -131,13 +151,17 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         let image:UIImage = UIImage.init(cgImage: cgImage)
         return image
     }
-    /*
-     @IBAction func sendToServer(_, sender:AnyObject){
-     let image:UIImage = convert(cmage: self.currentFrame)
-     let compressionRate:CGFloat = 1
-     let img = UIImageJPEGRepresentation(image, compressionRate)
-     }
-     */
+    
+    @objc func pred(){
+    print(currentFrame)
+      if(self.currentFrame != nil){
+        let img = buffer(from: currentFrame)
+        guard let captainPredOutput = try? model.prediction(image:img!) else {
+            fatalError("Unexpected runtime error.")
+        }
+      }
+    }
+    
     
 }
 
@@ -168,9 +192,3 @@ extension CMSampleBuffer {
     }
 }
 
-// AVCaptureVideoDataOutputSampleBufferDelegate protocol and related methods
-/*
-extension ViewController:  AVCaptureVideoDataOutputSampleBufferDelegate{
-
-}
-*/
